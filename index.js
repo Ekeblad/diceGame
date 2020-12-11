8cfe1d0d4c90b07c2046668c644c7be20a09c61f
Andreas Ekeblad <72961549+Ekeblad@users.noreply.github.com>
2020-12-11 08:44:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function randomizer () {
+  randomNumber1 = Math.floor(Math.random() * 6)+1;
+  randomNumber2 = Math.floor(Math.random() * 6)+1;
+
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  } else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  } else {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  }
+
+  diceRollPlayerOne(randomNumber1);
+  diceRollPlayerTwo(randomNumber2);
+}
+
+function diceRollPlayerOne (randomNumber1) {
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+}
+
+function diceRollPlayerTwo (randomNumber2) {
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+}
